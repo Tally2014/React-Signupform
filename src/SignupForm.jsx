7090178dfb200221f52ba7d01dd520eaf761a282
Montso Moalosi <montsoernestmoalosi@gmail.com>
2024-02-09 00:52:14 +0200

    Add HandleChange function

--- src/SignupForm.jsx
+++ src/SignupForm.jsx
@@ -3,6 +3,19 @@ import { useState } from "react";
 export default function SignupForm() {
   const [formData, setFormData] = useState({ firstName: "", lastName: "" });
 
+  const handleChange = (evt) => {
+    const changedField = evt.target.name;
+    const newValue = evt.target.value;
+    setFormData((currData) => {
+      currData[changedField] = newValue;
+      return { ...currData };
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(firstName, lastName);
+  };
+
   return (
     <div className="form">
       <label htmlFor="firstname">First Name</label>
@@ -10,6 +23,7 @@ export default function SignupForm() {
         type="text"
         placeholder="username"
         value={formData.firstName}
+        onChange={handleChange}
         id="firstname"
         name="firstName"
       />
@@ -18,10 +32,11 @@ export default function SignupForm() {
         type="text"
         placeholder="username"
         value={formData.lastName}
+        onChange={handleChange}
         id="lastname"
         name="lastName"
       />
-      <button>Submit</button>
+      <button onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
